a3581bf4e27c39a7e086a5078a965bb02b6633f9
Alvaro Roman <alvaroromanbengoechea@gmail.com>
2019-03-11 13:58:02 +0100

    Cambiado el Maps API Key. Quitado texto en caja de comentarios.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASEURL: '',
-  MAPS: 'AIzaSyCneU6o_5a6VhAT9Cm2l9eWOi3BYoAHjg4'
+  MAPS: 'AIzaSyARBExbvgz2Zl8KWGCp1ku_HwCA47-2PY8' //'AIzaSyCneU6o_5a6VhAT9Cm2l9eWOi3BYoAHjg4'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
     production: false,
     BASEURL: 'http://localhost:3000',
-    MAPS: 'AIzaSyCneU6o_5a6VhAT9Cm2l9eWOi3BYoAHjg4'
+    MAPS: 'AIzaSyARBExbvgz2Zl8KWGCp1ku_HwCA47-2PY8' //'AIzaSyCneU6o_5a6VhAT9Cm2l9eWOi3BYoAHjg4'
    };
