6a94d58cd992b547552b411b702459a6753370da
Alvaro Roman <alvaroromanbengoechea@gmail.com>
2019-08-07 15:54:16 +0200

    Added booking server entities.

---
+++ server/api/booking/booking.controller.js
@@ -0,0 +1,81 @@
+const _ = require("lodash");
+const debug = require("debug")("server:booking.controller");
+const Booking = require("./booking.model");
+const fields = Object.keys(_.omit(Booking.schema.paths, ["__v", "_id"]));
+
+const create = (req, res, next) =>
+{
+    const properties = _.pick(req.body, fields);
+    const newBooking = new Booking(properties);
+    
+    console.log(properties);
+
+    newBooking.save(err =>
+    {
+        if (err)
+        {
+            debug(err);
+            res.status(400).json({ message: "Something went wrong when trying to create the booking", error: err });
+        }
+        else
+        {
+            res.status(200).json(newBooking);
+        }
+    });
+};
+
+/*const getOne = (req, res, next) => {
+  Review.findById(req.params.id)
+    .populate("author")
+    .exec()
+    .then(review => {
+      res.status(200).json({ review });
+    })
+    .catch(err => {
+      debug(err);
+      res.status(400).json({ message: "Review not found" });
+    });
+};
+
+const create = (req, res, next) => {
+  const properties = _.pick(req.body, fields);
+  const newReview = new Review(properties);
+
+  newReview.save(err => {
+    if (err) {
+      debug(err);
+      res
+        .status(400)
+        .json({ message: "Something went wrong when trying to create review" });
+    } else {
+      res.status(201).json(newReview);
+    }
+  });
+};
+
+const update = (req, res, next) => {
+  // Idea: Reviews can't be updated
+  const updates = _.pick(req.body, fields);
+
+  Review.findByIdAndUpdate(req.params.id, updates)
+    .then(review => {
+      res.status(200).json({ message: "Review updated." });
+    })
+    .catch(err => {
+      debug(err);
+      res.status(400).json({ message: "Error updating review" });
+    });
+};
+
+const erase = (req, res, next) => {
+  Review.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "Review removed" });
+    })
+    .catch(err => {
+      debug(err);
+      res.status(400).json({ message: "Error erasing review" });
+    });
+};*/
+
+module.exports = { create };
