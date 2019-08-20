107f4320b13ad3c2a283c31c5d4eea21d54faea4
Alvaro Roman <alvaroromanbengoechea@gmail.com>
2019-08-20 16:58:06 +0200

    Added emails to bookings.

--- server/api/booking/booking.controller.js
+++ server/api/booking/booking.controller.js
@@ -1,81 +1,149 @@
 const _ = require("lodash");
 const debug = require("debug")("server:booking.controller");
 const Booking = require("./booking.model");
+const Course = require("../course/course.model");
 const fields = Object.keys(_.omit(Booking.schema.paths, ["__v", "_id"]));
+const nodemailer = require('nodemailer');
+const { transporter } = require("../../config/nodemailer/transporter");
+var ejs = require("ejs");
+var path = require("path");
+var moment = require('moment');
 
 const create = (req, res, next) =>
 {
     const properties = _.pick(req.body, fields);
     const newBooking = new Booking(properties);
     
-    console.log(properties);
-
-    newBooking.save(err =>
+    Course.findOne({_id: newBooking.course }).populate('academy').exec(function(courseErr, course)
     {
-        if (err)
+        if (courseErr)
         {
-            debug(err);
-            res.status(400).json({ message: "Something went wrong when trying to create the booking", error: err });
+            console.log("courseErr: ", courseErr);
+            res.status(400).json({ message: "Something went wrong when trying to create the booking", error: courseErr });
         }
         else
-        {
-            res.status(200).json(newBooking);
+        {    
+            newBooking.save(err =>
+            {
+                if (err)
+                {
+                    debug(err);
+                    res.status(400).json({ message: "Something went wrong when trying to create the booking", error: err });
+                }
+                else
+                {
+                    ejs.renderFile(path.resolve(__dirname + "/../../config/nodemailer/templates/booking_user.ejs"),
+                    {
+                        name: newBooking.name,
+                        group: newBooking.group,
+                        courseTitle: course.title,
+                        coursePrice: course.price,
+                        courseStartDate: moment(course.startDate).locale("es").format("dddd D MMMM"),
+                        academyName: course.academy.name.toUpperCase(),
+                        academyAddress: course.academy.address
+                    },
+                    function (error, data)
+                    {
+                        if (error)
+                        {
+                            console.log('Message error: %s', error);
+                            
+                            res.status(400).json({ message: "Something went wrong when trying to create the booking", error: error })
+                        }
+                        else
+                        {
+                            // Send user email
+                            var userEmailOptions =
+                            {
+                                from: '"Appcademos" <hello@appcademos.com>', // sender address
+                                to: [newBooking.email],
+                                subject: 'Reserva confirmada ✔', // Subject line
+                                html: data,
+                                attachments: [
+                                {
+                                    filename: 'appcademos_facebook.png',
+                                    path: path.resolve(__dirname + '/../../config/nodemailer/images/facebook.png'),
+                                    cid: 'facebook@appcademos'
+                                },
+                                {
+                                    filename: 'appcademos_twitter.png',
+                                    path: path.resolve(__dirname + '/../../config/nodemailer/images/twitter.png'),
+                                    cid: 'twitter@appcademos'
+                                },
+                                {
+                                    filename: 'appcademos_instagram.png',
+                                    path: path.resolve(__dirname + '/../../config/nodemailer/images/instagram.png'),
+                                    cid: 'instagram@appcademos'
+                                },
+                                {
+                                    filename: 'appcademos_web.png',
+                                    path: path.resolve(__dirname + '/../../config/nodemailer/images/link.png'),
+                                    cid: 'link@appcademos'
+                                }]
+                            }
+                            transporter.sendMail(userEmailOptions, function (e, info)
+                            {
+                                if (e)
+                                {
+                                    console.log('Message error: %s', e);
+                                    
+                                    res.status(400).json({ message: "Something went wrong when trying to create the booking", error: e })
+                                }
+                                else
+                                {
+                                    console.log('Message sent: %s', JSON.stringify(info));
+                                    
+                                    res.status(200).json(newBooking);
+                                }
+                            });
+                            
+                            ejs.renderFile(path.resolve(__dirname + "/../../config/nodemailer/templates/booking_team.ejs"),
+                            {
+                                name: newBooking.name,
+                                phone: newBooking.phone,
+                                email: newBooking.email,
+                                group: newBooking.group,
+                                courseId: course._id,
+                                courseTitle: course.title,
+                                coursePrice: course.price,
+                                academy: course.academy.name,
+                                academyAddress: course.academy.address
+                            },
+                            function (errorTeam, dataTeam)
+                            {
+                                if (errorTeam)
+                                {
+                                    console.log('Team Message error: %s', errorTeam);
+                                }
+                                else
+                                {
+                                    // Send team email
+                                    var teamEmailOptions =
+                                    {
+                                        from: '"Appcademos" <hello@appcademos.com>',
+                                        to: ['hello@appcademos.com'],
+                                        subject: '¡CLINC, CLINC, CLINC! 😎',
+                                        html: dataTeam
+                                    }
+                                    transporter.sendMail(teamEmailOptions, function (ee, infoo)
+                                    {
+                                        if (ee)
+                                        {
+                                            console.log('Team email Message error: %s', ee);
+                                        }
+                                        else
+                                        {
+                                            console.log('Team email Message sent: %s', JSON.stringify(infoo));
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
         }
     });
 };
-
-/*const getOne = (req, res, next) => {
-  Review.findById(req.params.id)
-    .populate("author")
-    .exec()
-    .then(review => {
-      res.status(200).json({ review });
-    })
-    .catch(err => {
-      debug(err);
-      res.status(400).json({ message: "Review not found" });
-    });
-};
-
-const create = (req, res, next) => {
-  const properties = _.pick(req.body, fields);
-  const newReview = new Review(properties);
-
-  newReview.save(err => {
-    if (err) {
-      debug(err);
-      res
-        .status(400)
-        .json({ message: "Something went wrong when trying to create review" });
-    } else {
-      res.status(201).json(newReview);
-    }
-  });
-};
-
-const update = (req, res, next) => {
-  // Idea: Reviews can't be updated
-  const updates = _.pick(req.body, fields);
-
-  Review.findByIdAndUpdate(req.params.id, updates)
-    .then(review => {
-      res.status(200).json({ message: "Review updated." });
-    })
-    .catch(err => {
-      debug(err);
-      res.status(400).json({ message: "Error updating review" });
-    });
-};
-
-const erase = (req, res, next) => {
-  Review.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(200).json({ message: "Review removed" });
-    })
-    .catch(err => {
-      debug(err);
-      res.status(400).json({ message: "Error erasing review" });
-    });
-};*/
 
 module.exports = { create };
