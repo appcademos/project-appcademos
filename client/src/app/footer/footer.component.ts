352ee98c21bce2fc21f2e8081b91af611d14a90b
Alvaro Roman <alvaroromanbengoechea@gmail.com>
2020-01-08 22:25:20 +0100

    Configurar entorno para A/B testing

--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit
+{
+    AB_TESTING_ENV = (window as any).__env.AB_TESTING_ENV;
+    
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() { }
+    
 }
---
+++ client/src/env_example.js
@@ -0,0 +1,6 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  window.__env.AB_TESTING_ENV = 'B';
+  
+}(this));
