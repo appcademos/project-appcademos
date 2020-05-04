dba6fd4b72bf91d265175b9e576f7aef44de2840
Alvaro Roman <alvaroromanbengoechea@gmail.com>
2020-05-04 15:27:05 +0200

    Added create category endpoint

--- server/api/category/category.js
+++ server/api/category/category.js
@@ -4,10 +4,12 @@ const Utils = require("../utils");
 
 const {
   getAll,
+  create,
   update
 } = require("./category.controller");
 
 router.get("/", [ Utils.hasAccess('adminAndAcademy'), getAll ]);
+router.post("/", [ Utils.hasAccess('admin'), create ]);
 router.post("/:id/update", [ Utils.hasAccess('admin'), update ]);
 
 module.exports = router;
