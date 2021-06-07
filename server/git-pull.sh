103cbba07b3ec6066ffaac8c2d83e0098c4435c7
Alvaro Roman <alvaro@homyhub.com>
2021-06-07 14:35:37 +0200

    Updated sit-pull commands

--- server/git-pull.sh
+++ server/git-pull.sh
@@ -2,7 +2,7 @@ cd /var/www/appcademos.com/project-appcademos
 git pull origin master
 cd server
 npm install
-#pm2 reload appcademos
-pm2 stop appcademos
-pm2 start npm --name "appcademos-server" -- start
-pm2 start dist/server/main.js --name "appcademos-client-ssr"
+pm2 reload appcademos-server
+pm2 reload appcademos-client-ssr
+#pm2 start npm --name "appcademos-server" -- start
+#pm2 start dist/server/main.js --name "appcademos-client-ssr"
